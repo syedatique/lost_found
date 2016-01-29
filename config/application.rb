9a5c3aba467112df9bf8187492d4d7cc242fe31f
syed <syed.atique@gmail.com>
2016-01-29 22:13:32 +0000

    config/application.rb changed

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,10 @@ module LostFound
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # to support heroku
+    config.serve_static_assets = true
+
+
   end
 end
